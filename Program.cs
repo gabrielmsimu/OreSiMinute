b6a7dd9570685d7fa38e428a680255d95f7361de
gabrielmsimu <72418738+gabrielmsimu@users.noreply.github.com>
2020-10-30 22:14:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OreSiMinute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti ora curenta");
+            int h = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti minutele curente");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti cate minute doriti sa adaugati");
+            int x = int.Parse(Console.ReadLine());
+
+            int m2 = (m + x) % 60;
+            int h2 = h + x / 60;
+
+            Console.WriteLine($"Peste {x} minute va fi ora {h2} si {m2} minute.");
+
+        }
+    }
+}
